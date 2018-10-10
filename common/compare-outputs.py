6545b8a5e239bc70d63dd5ea33cd789331d36fbb
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2018-10-10 11:10:56 +0200

    Updated compare-outputs.py

--- common/compare-outputs.py
+++ common/compare-outputs.py
@@ -1,51 +1,50 @@
-# read MATLAB output
-with open('outputFromMatlab.txt', 'r') as matlabFile:
-    matlabData = []
-    for line in matlabFile:
-        line = line.split()
-        line = [int(i) for i in line]
-        matlabData += line
+import sys
 
-# read Butterlfy output
-with open('outputFromButterfly.txt', 'r') as butterflyFile:
-    butterflyData = []
-    for line in butterflyFile:
+# check if both files are given as arguments
+if len(sys.argv) != 3:
+    print('Error: not enough input arguments.')
+    print('Usage: python compare-ouputs.py <file1> <file2>')
+    sys.exit(1)
+
+# read file1
+with open(sys.argv[1], 'r') as file1:
+    file1Data = []
+    for line in file1:
         line = line.split()
         line = [int(i) for i in line]
-        butterflyData += line
+        file1Data += line
+
+# read file2
+with open(sys.argv[2], 'r') as file2:
+    file2Data = []
+    for line in file2:
+        line = line.split()
+        line = [int(i) for i in line]
+        file2Data += line
 
 # compare number by number the two outputs
 matchCounter = 0
 errorLines = []
-for i, j in zip(matlabData, butterflyData):
+for i, j in zip(file1Data, file2Data):
     if i == j:
         matchCounter += 1
     else:
-        errStr = "Errore al dato {} (riga {})".format(
-            butterflyData.index(j), (butterflyData.index(j)//4) + 1)
+        errStr = "Error at line {}".format(file2Data.index(j))
         # save lines that generated an error
-        errorLines.append((butterflyData.index(j)//4))
+        errorLines.append(file2Data.index(j))
         print(errStr)
 
 # print results
-outStr = "Numero di match: {}/{} ({}%)".format(matchCounter,
-                                               len(butterflyData), 100*matchCounter/len(butterflyData))
+outStr = "Total matches: {}/{} ({}%)".format(matchCounter, len(file2Data), 100*matchCounter/len(file2Data))
 print(outStr)
-if matchCounter == len(butterflyData):
+if matchCounter == len(file2Data):
     print("Yeeeeee!!!")
 
-# if there are error lines, save them in a file
+# save samples that generate errors
 if errorLines:
     i = 0
-    with open('inputVectors.txt', 'r') as inputs:
-        with open('error_inputVectors.txt', 'a') as errFile:
-            for line in inputs:
-                if i in errorLines:
-                    errFile.write(line)
-                i += 1
-    i = 0
-    with open('outputFromMatlab.txt', 'r') as inputs:
-        with open('error_outputFromMatlab.txt', 'a') as errFile:
+    with open('samples.txt', 'r') as inputs:
+        with open('error_samples.txt', 'w') as errFile:
             for line in inputs:
                 if i in errorLines:
                     errFile.write(line)
