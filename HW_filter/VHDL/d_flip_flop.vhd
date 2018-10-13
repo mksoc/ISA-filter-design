5854739b79dae621c0bd54f9ba855dcb12fab2b9
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2018-10-13 13:43:47 +0200

    Renamed d_flip_flop.vhd

--- HW_filter/VHDL/d_flip_flop.vhd
+++ HW_filter/VHDL/d_flip_flop.vhd
@@ -1,15 +1,15 @@
--- file 'ff_d.vhd'
+-- file 'd_flip_flop.vhd'
 -- enable and async clear
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity ff_d is
+entity d_flip_flop is
     port (D: in std_logic;
           clock, clear, enable: in std_logic;
           Q: out std_logic;
-end ff_d;
+end d_flip_flop;
 
-architecture behavior of ff_d is
+architecture behavior of d_flip_flop is
 begin
     process (clock, clear)
     begin
