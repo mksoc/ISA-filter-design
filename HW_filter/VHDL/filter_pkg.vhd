542a0db9b0b06acec57e61f64936decb5907b311
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2018-10-14 10:34:28 +0200

    Removed flip flop from package

--- HW_filter/VHDL/filter_pkg.vhd
+++ HW_filter/VHDL/filter_pkg.vhd
@@ -2,23 +2,16 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- NB : parallelism of data of the filter
--- N : order of the filter
--- bCoeffType, aCoeffType : compact form to express the coefficients. They depend on N
-
 package filter_pkg is
-	constant NB : natural := 12;
-	constant N : positive := 3;
+	-- constant declarations
+	constant NB : natural := 12; -- number of bits of data
+	constant N : positive := 3; -- filter order
 
+	-- types declarations
 	type bCoeffType is array (0 to N-1) of signed(NB-1 downto 0);
 	type aCoeffType is array (1 to N-1) of signed(NB-1 downto 0);
 
-	component d_flip_flop is
-		port (D: in std_logic;
-			  clock, clear, enable: in std_logic;
-			  Q: out std_logic;
-	end component;
-
+	-- components declarations
 	component reg is
 		generic (N: positive := 8);
 		port (D: in std_logic_vector(N-1 downto 0);
