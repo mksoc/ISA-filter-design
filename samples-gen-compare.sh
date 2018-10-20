194a239398dd79684b3a37bca4cc0340528d078f
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2018-10-20 15:45:11 +0200

    Updated samples and results

--- samples-gen-compare.sh
+++ samples-gen-compare.sh
@@ -14,39 +14,36 @@ PORT="10020"
 SSH_SOCKET=~/".ssh/$USER_HOST"
 REMOTE_ROOT="/home/isa22/lab1"
 
-# generate samples with Python script
 echo "Running samples generator..."
 cd common
 python3 samples-generator.py 
 
-# rename samples file
 echo "Renaming samples file..."
 cp py-samples.txt samples.txt
 
-# run C model
 echo "Running C model..."
 cd ../C_filter
 ./iir_filter.exe ../common/samples.txt ../common/results-c.txt
 
-# create master SSH connection
+echo "Connecting to server..."
 ssh -M -f -N -o ControlPath="$SSH_SOCKET" -p $PORT "$USER_HOST"
 
-# copy new samples onto server
+echo "Copying samples to server..."
 cd ..
 scp -o ControlPath="$SSH_SOCKET" -P $PORT common/samples.txt "$USER_HOST":"$REMOTE_ROOT"/common
 
-# connect to server and run HW simulation
+echo "Running simulation..."
 ssh -S "$SSH_SOCKET" -p $PORT "$USER_HOST" "cd lab1/sim && vsim -c -do simulation_script.tcl"
 
-# retrieve results from server
+echo "Copying results from server..."
 scp -o ControlPath="$SSH_SOCKET" -P $PORT "$USER_HOST":"$REMOTE_ROOT"/common/results-hw.txt common/
 
-# close master connection
+echo "Closing connection..."
 ssh -S "$SSH_SOCKET" -O exit "$USER_HOST"
 
-# commit changes
+echo "Committing changes..."
 git commit -am "Updated samples and results"
 
-# compare results
+echo "Comparing results..."
 cd common
 python3 compare-outputs.py results-c.txt results-hw.txt
