49a4f60f0400008d101881aec888354a8a3fa444
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2018-10-26 11:34:34 +0200

    Created syn-script.tcl

---
+++ HW_filter/code/syn/syn-script.tcl
@@ -0,0 +1,37 @@
+# to be run inside /home/isa22/lab1/syn
+# compile the needed project files
+set dir "../src"
+set source_files [glob -directory $dir *.vhd]
+foreach item $source_files {
+    analyze -f vhdl -lib WORK $item
+} 
+
+# preserve RTL names
+set power_preserve_rtl_hier_names true
+
+# elaborate design
+elaborate iir_filter -arch structure -lib WORK > ./elaborate-log.txt
+uniquify
+link
+
+# create symbolic clock signal
+create_clock -name CLOCK -period 10.0 clk
+set_dont_touch_network CLOCK
+set_clock_uncertainty 0.07 [get_clocks CLOCK]
+
+# set input/output delays
+set_input_delay 0.5 -max -clock CLOCK [remove_from_collection [all_inputs] CLOCK]
+set_output_delay 0.5 -max -clock CLOCK [all_outputs]
+
+# set output load capacitance
+set OLOAD [load_of NangateOpenCellLibrary/BUF_X4/A]
+set $OLOAD [all_outputs]
+
+# start synthesis
+compile > ./compile-log.txt
+
+# save results
+report_timing > timing-report.txt
+report_area > area-report.txt
+
+quit
