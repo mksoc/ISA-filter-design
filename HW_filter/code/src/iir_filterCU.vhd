6c47c220bcf01fd75bf32c91f7f56280a7ad4151
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2018-10-26 17:50:15 +0200

    Removed RESET state

--- HW_filter/code/src/iir_filterCU.vhd
+++ HW_filter/code/src/iir_filterCU.vhd
@@ -7,21 +7,21 @@ entity iir_filterCU is
         clk, rst_n: in std_logic;
         vIn: in std_logic;
         -- controls to DP
-        regs_clr, input_regs_en, sw_out_regs_en: out std_logic;
+        input_regs_en, sw_out_regs_en: out std_logic;
         -- to external world
         vOut : out std_logic 
     );
 end entity;
 
 architecture behavior of iir_filterCU is
-	type stateType is (RESET, IDLE, FIRST_SAMPLE, SAMPLE, PAUSE);
+	type stateType is (IDLE, FIRST_SAMPLE, SAMPLE, PAUSE);
 	signal presentState, nextState: stateType;
 
 begin
     ps_process: process (clk, rst_n)
         begin
             if (rst_n = '0') then
-                presentState <= RESET;
+                presentState <= IDLE;
             else
                 if (clk'event and clk = '1') then
                     presentState <= nextState;
@@ -32,9 +32,6 @@ begin
     ns_process: process (presentState, vIn)
         begin
             case presentState is
-                when RESET =>
-                    nextState <= IDLE;
-
                 when IDLE =>
                     if (vIn = '1') then
                         nextState <= FIRST_SAMPLE;
@@ -64,21 +61,17 @@ begin
                     end if;  
 
                 when others => 
-                    nextState <= RESET;
+                    nextState <= IDLE;
             end case;
         end process;
 
     out_process: process(presentState)
         begin
-            -- default assignments        
-            regs_clr <= '0';
+            -- default assignments  
             sw_out_regs_en <= '0';
             vOut <= '0';
 
             case presentState is
-                when RESET => 
-                    regs_clr <= '1';
-
                 when IDLE =>
 
                 when FIRST_SAMPLE => 
